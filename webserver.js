196c7fd0bc5ff5b32877d87f593a786f5f6fd188
Mika Petak <git@mika-petak.de>
2025-01-03 15:06:58 +0100

    adding http

--- webserver.js
+++ webserver.js
@@ -2,6 +2,7 @@ import {handler} from './build/handler.js';
 import express from 'express';
 import https from 'https';
 import fs from 'node:fs';
+import * as http from 'node:http';
 
 const privateKey = fs.readFileSync('./certs/privkey1.pem', 'utf8');
 const certificate = fs.readFileSync('./certs/fullchain1.pem', 'utf8');
@@ -9,12 +10,18 @@ const credentials = {key: privateKey, cert: certificate};
 
 const app = express();
 
+const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
 
+const PORT = 80;
 const SSLPORT = 443;
 
+httpServer.listen(PORT, function() {
+	console.log('HTTP Server is running on port', PORT)
+})
+
 httpsServer.listen(SSLPORT, function () {
-	console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
+	console.log('HTTPS Server is running on port', SSLPORT);
 });
 
 // add a route that lives separately from the SvelteKit app
