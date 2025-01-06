6dc2d1898b1c713a59f320dcb5f336b4e93f1ab5
Mika Petak <git@mika-petak.de>
2025-01-06 02:41:53 +0100

    overlay home button

---
+++ src/routes/capture/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { HomeOutline } from 'flowbite-svelte-icons';
+
+	let { children } = $props();
+</script>
+
+<a class="bg-blue-600 rounded-lg h-12 w-12 absolute bottom-6 left-6 opacity-60 drop-shadow-xl" href="/">
+	<HomeOutline class="text-white" />
+</a>
+
+{@render children()}
