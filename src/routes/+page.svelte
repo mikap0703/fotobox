a0accef86899ecc4d1fec82d57f98d6b8240db23
Mika Petak <git@mika-petak.de>
2025-01-06 01:08:45 +0100

    landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="h-screen w-screen bg-blue-600 flex flex-col items-center">
+	<div class="relative w-full h-full flex items-center justify-center">
+		<h1 class="text-white font-bold text-9xl">Fotobox</h1>
+	</div>
+
+	<div class="w-full h-full flex items-center justify-evenly">
+		<a
+			href="/capture"
+			class="bg-gray-50 h-64 px-16 rounded-2xl text-7xl font-bold shadow-lg hover:scale-105 duration-200 flex items-center justify-center"
+		>
+			Fotos aufnehmen
+		</a>
+
+		<a
+			href="/settings"
+			class="bg-gray-50 h-64 px-16 rounded-2xl text-7xl font-bold shadow-lg hover:scale-105 duration-200 flex items-center justify-center"
+		>
+			Einstellungen
+		</a>
+	</div>
+</div>
