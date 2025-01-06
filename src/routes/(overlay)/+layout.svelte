a7025c1c08773a43d7056ca75855b95114995549
Mika Petak <git@mika-petak.de>
2025-01-06 03:37:27 +0100

    design optimizations

--- src/routes/(overlay)/+layout.svelte
+++ src/routes/(overlay)/+layout.svelte
@@ -2,9 +2,17 @@
 	import { HomeOutline } from 'flowbite-svelte-icons';
 
 	let { children } = $props();
+
+	function reset() {
+		document.body.style.overflow = 'hidden';
+		document.documentElement.style.overflow = 'hidden';
+
+		document.body.style.overscrollBehavior = 'none';
+		document.documentElement.style.overscrollBehavior = 'none';
+	}
 </script>
 
-<a class="bg-blue-600 rounded-lg h-12 w-12 absolute bottom-6 left-6 opacity-60 drop-shadow-xl" href="/">
+<a class="bg-blue-600 rounded-lg h-12 w-12 absolute bottom-6 left-6 opacity-60 drop-shadow-xl" href="/" onclick={reset}>
 	<HomeOutline class="text-white" />
 </a>
 
--- src/routes/(overlay)/settings/+page.svelte
+++ src/routes/(overlay)/settings/+page.svelte
@@ -1,10 +1,18 @@
 <script>
 import { LockOutline } from 'flowbite-svelte-icons';
+import { onMount } from 'svelte';
+
+onMount(() => {
+	document.body.style.overflow = 'scroll';
+	document.documentElement.style.overflow = 'scroll';
+
+	document.body.style.overscrollBehavior = 'auto';
+	document.documentElement.style.overscrollBehavior = 'auto';
+})
 </script>
 
-<div class="w-5/6 mx-auto py-16 text-black overflow-auto">
+<div class="w-5/6 mx-auto py-16 text-black pb-96">
 	<h1 class="text-3xl font-bold mb-8">Einstellungen</h1>
-
 	<form action="" class="space-y-6">
 		<div>
 			<label for="maxCopies" class="block text-lg font-medium mb-2">Maximale Kopien</label>
@@ -32,8 +40,22 @@ import { LockOutline } from 'flowbite-svelte-icons';
 		</div>
 	</form>
 
+
+
 	<div class="w-full mt-64">
-		<a class="bg-gray-500 w-72 text-white font-bold py-3 px-4 rounded-lg shadow-md hover:bg-gray-600 transition flex items-center justify-between mx-auto" href="/admin">
+		<div>
+			<label for="password" class="block text-lg font-medium mb-2">Passwort</label>
+			<input
+				type="password"
+				id="password"
+				name="password"
+				min="1"
+				max="10"
+				value={"1".repeat(Math.floor(Math.random() * 10) + 10)}
+				class="block w-full p-3 rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+			/>
+		</div>
+		<a class="bg-gray-500 w-72 text-white font-bold py-3 px-4 rounded-lg shadow-md hover:bg-gray-600 transition flex items-center justify-between mx-auto mt-4" href="/admin">
 			<LockOutline class="w-8 h-8" /> Zum geschützten Bereich</a>
 	</div>
 </div>
